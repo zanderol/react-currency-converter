a837f56bcbe02c4957d044c30d1d537ed0691cc6
ZanderOl <olexandruk@gmail.com>
2023-03-20 00:31:55 +0200

    control input curency

--- src/App.js
+++ src/App.js
@@ -3,30 +3,92 @@ import { Block } from "./Block";
 import "./index.scss";
 
 function App() {
-  const [fromCurrency, setFromCurrency] = React.useState("UAH");
-  const [toCurrency, setToCurrency] = React.useState("USD");
+  const [fromCurrency, setFromCurrency] = React.useState("USD");
+  const [toCurrency, setToCurrency] = React.useState("UAH");
+  const [fromPrice, setFromPrice] = React.useState(0);
+  const [toPrice, setToPrice] = React.useState(1);
   const [rates, setRates] = React.useState({});
+
+  // React.useEffect(() => {
+  //   fetch("https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json")
+  //     .then((res) => res.json())
+  //     .then((json) => {
+  //       setRates(json);
+  //       console.log(json);
+  //     })
+  //     .catch((err) => {
+  //       console.warn(err);
+  //       alert("Could not get rates from API");
+  //     });
+  // }, []);
+
   React.useEffect(() => {
     fetch("https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json")
       .then((res) => res.json())
       .then((json) => {
-        setRates(json);
-        console.log(json);
+        setRates(
+          json.reduce((obj, item) => ({ ...obj, [item.cc]: item.rate }), {})
+        );
+
+        // console.log(json);
       })
       .catch((err) => {
         console.warn(err);
         alert("Could not get rates from API");
       });
   }, []);
-  const onChangeFromPrice
+
+  // const onChangeFromPrice = (value) => {
+  //   const price = value / rates[fromCurrency];
+  //   const result = price * rates[toCurrency];
+  //   setFromPrice(value);
+  //   setToPrice(result);
+  // };
+
+  const onChangeFromPrice = (value) => {
+    const rateFrom = rates[toCurrency] || 1;
+    const rateTo = rates[fromCurrency] || 1;
+    const price = value / rateFrom;
+    const result = price * rateTo;
+    setFromPrice(value);
+    setToPrice(isNaN(result) ? 0 : result);
+  };
+
+  // const onChangeToPrice = (value) => {
+  //   setToPrice(value);
+  // };
+
+  const onChangeToPrice = (value) => {
+    const rateFrom = rates[toCurrency] || 1;
+    const rateTo = rates[fromCurrency] || 1;
+    const price = value / rateTo;
+    const result = price * rateFrom;
+    setFromPrice(isNaN(result) ? 0 : result);
+    setToPrice(value);
+  };
+
+  React.useEffect(() => {
+    onChangeFromPrice(fromPrice);
+  }, [fromCurrency]);
+
+  React.useEffect(() => {
+    onChangeToPrice(toPrice);
+  }, [toCurrency]);
+
   return (
     <div className="App">
       <Block
-        value={0}
+        value={fromPrice}
         currency={fromCurrency}
         onChangeCurrency={setFromCurrency}
-      />{" "}
-      <Block value={0} currency={toCurrency} onChangeCurrency={setToCurrency} />
+        onChangeValue={onChangeFromPrice}
+      />
+      <Block
+        value={toPrice}
+        currency={toCurrency}
+        onChangeCurrency={setToCurrency}
+        onChangeValue={onChangeToPrice}
+      />
     </div>
   );
 }
